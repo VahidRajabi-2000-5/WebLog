fef3cc0b20bf17bcfffcbf7fb60de8eae9d64a2f
vahid <vahidrajabi.pro7913@gmail.com>
2023-07-10 20:49:37 +0330

    Create model post show in Panel admin

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'author',
+        'datetime_created',
+        'datetime_modified',
+        'status',
+
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+ 
