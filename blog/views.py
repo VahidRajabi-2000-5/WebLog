aaafde9b954ae41ad6f66e411299d5487f5a06e7
vahid <vahidrajabi.pro7913@gmail.com>
2023-07-13 17:39:55 +0330

    Create PostCreateView with Infrastructure Create view in DB

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
+
 
 from .models import Post
 
@@ -12,6 +14,36 @@ def post_detail_view(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{"post":post})
     
-    
+# =======================
 # def handling_404(request,exception):
-#     return render (request,'blog/404.html')
+#     return render (request,'blog/404.html') 
+# =======================
+
+def post_create_view(request):
+    if request.method =="POST":
+        print("POST Request")
+        post_title = request.POST.get('title')
+        post_text = request.POST.get('text')
+        
+        user = User.objects.all()[0]
+        
+        Post.objects.create(title=post_title,text=post_text,author=user,status='Pub')
+    
+    else:
+        print("GET Request")
+        
+    return render(request,'blog/post_create.html',)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
